b86751a90c200f9766fa68bd7c672892a976815b
Johnny Viel <johnny.viel@syccuxas01.pcc.edu>
2016-03-12 10:44:03 -0800

    complete recursive permute function

--- permute.cpp
+++ permute.cpp
@@ -2,42 +2,48 @@
 
 using namespace std;
 
-void permute( int* arr, int &sz, int &num);
+void permute( int* arr, const int &sz, int num);
+void printarr( int*& arr, const int &sz );
+void swapCols( int*& arr, const int &sz, int num );
 
 int main()
 {
-    int sz = 5;
-    int num = 5;
-    int arr[5] = {1,2,3,4,5};
+    const int sz     = 5;
+    int       num    = 5;
+    int       arr[5] = {1,2,3,4,5};
+
     permute( arr, sz, num );
+    
+    return 0;
 }
 
-void permute( int* arr, int &sz, int &num )
+void permute( int * arr, const int &sz, int num )
 {
-    if( num == 1 ){
-        for( int i=0; i<sz; i++ )
-            cout << arr[i] << " ";
-        cout << endl;
-    }   
-    if( num == 2 ){
-        // print full string
-        for( int i=0; i<sz; i++ )
-            cout << arr[i] << " ";
-        cout << endl;
-        // swap, print
-        int temp = arr[sz-num];
-        arr[sz-num] = arr[sz-num+1];
-        arr[sz-num+1] = temp;
-        for( int i=0; i<sz; i++ )
-            cout << arr[i] << " ";
-        cout << endl;
-        // swap again
-        temp = arr[sz-num];
-        arr[sz-num] = arr[sz-num+1];
-        arr[sz-num+1] = temp;
+    if( num == 1 )
+        printarr( arr, sz );
+
+    for( int i=0; i<num; i++ )
+    {
+        permute( arr, sz, (num-1) );
+        swapCols( arr, sz, num );
     }
-    else
-        permute( arr, sz, --num );
 
     return;
+}
+
+void printarr( int*& arr, const int &sz )
+{
+    for( int i=0; i<sz; i++ )
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void swapCols( int *& arr, const int &sz, int num )
+{
+        int pivot = sz-num;
+        int temp  = arr[pivot];
+     // shift one-by-one to the left
+        for( int j=pivot; j<sz-1; j++ )
+            arr[j] = arr[j+1]; 
+        arr[sz-1] = temp;         // set last as first
 }
